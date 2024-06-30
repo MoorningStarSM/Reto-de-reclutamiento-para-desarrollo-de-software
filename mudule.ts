d3f7a5791c005420c7b749e7f70362357193ea15
Enrique Eduardo Lozano Aceves <163630204+MoorningStarSM@users.noreply.github.com>
2024-06-30 01:34:10 -0600

    Create mudule.ts

---
+++ mudule.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
